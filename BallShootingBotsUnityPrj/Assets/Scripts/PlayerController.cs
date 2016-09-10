ebe9fca3b20c51780edf4afb0e0d659034ef990d
Thunder0ne <thuner0@email.it>
2016-09-10 11:07:07 +0100

    first commit Fixed movement of agent (uncheck is kinematic flag in editor)

---
+++ BallShootingBotsUnityPrj/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Rigidbody rb;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertival = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertival);
+
+        rb.AddForce(movement * speed);
+	}
+}
