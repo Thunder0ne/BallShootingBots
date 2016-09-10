a09055b5758daec6714367a61e39a562f72124d5
Thunder0ne <thuner0@email.it>
2016-09-11 00:00:30 +0100

    implemented script to test the locomotion model

--- BallShootingBotsUnityPrj/Assets/Scripts/PlayerController.cs
+++ BallShootingBotsUnityPrj/Assets/Scripts/PlayerController.cs
@@ -3,22 +3,31 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float maxForce = 5.0f;
+    public float maxTorque = 2.0f;
 
     private Rigidbody rb;
+    private float groundHeight;
 
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
-	}
-	
-	void FixedUpdate ()
+        groundHeight = rb.position.y;
+    }
+
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertival = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertival);
+        //compensating position and orientation noise if any
+        Vector3 pos = rb.position;
+        pos.y = groundHeight;
+        Quaternion straightUpRotation = Quaternion.Euler(0, rb.transform.rotation.eulerAngles.y, 0);
+        rb.MovePosition(pos);
+        rb.MoveRotation(straightUpRotation);
 
-        rb.AddForce(movement * speed);
-	}
+        rb.AddRelativeForce(0.0f, 0.0f, moveVertival * maxForce);
+        rb.AddRelativeTorque(0.0f, moveHorizontal * maxTorque, 0.0f);
+    }
 }
